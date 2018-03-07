f54ced960007f4b131d8619af6c9213571cbb0d4
victorminerva <victorminerva.m@gmail.com>
2018-03-06 21:58:36 -0300

    Creating the Service fields

---
+++ src/main/java/br/com/minervait/spring/service/PessoaServiceImpl.java
@@ -0,0 +1,39 @@
+package br.com.minervait.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.minervait.spring.dao.PessoaDao;
+import br.com.minervait.spring.model.Pessoa;
+
+@Service
+@Transactional(readOnly = false)
+public class PessoaServiceImpl implements PessoaService {
+
+	@Autowired
+	private PessoaDao pessoaDao;
+
+	public long save(Pessoa pessoa) {
+		return pessoaDao.save(pessoa);
+	}
+
+	public Pessoa get(long id) {
+		return pessoaDao.get(id);
+	}
+
+	public List<Pessoa> list() {
+		return pessoaDao.list();
+	}
+
+	public void update(long id, Pessoa pessoa) {
+		pessoaDao.update(id, pessoa);
+	}
+
+	public void delete(long id) {
+		pessoaDao.delete(id);
+	}
+
+}
