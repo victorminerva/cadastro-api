aa73a5252daa32a323e7d21ff05dc34d1e103822
victorminerva <victorminerva.m@gmail.com>
2018-03-06 21:56:40 -0300

    Creating the configuration fields

---
+++ src/main/java/br/com/minervait/spring/config/ServletConfig.java
@@ -0,0 +1,13 @@
+package br.com.minervait.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "br.com.minervait.spring.controller" })
+public class ServletConfig extends WebMvcConfigurerAdapter {
+
+}
