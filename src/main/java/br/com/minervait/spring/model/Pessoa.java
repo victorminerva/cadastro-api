1d19dba4a3a13d237d5909d8bcf15a179c74e7ff
victorminerva <victorminerva.m@gmail.com>
2018-03-07 21:05:04 -0300

    Adjusting the parameter name to uppercase to create the table in database

--- src/main/java/br/com/minervait/spring/model/Pessoa.java
+++ src/main/java/br/com/minervait/spring/model/Pessoa.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "Pessoa")
+@Entity(name = "PESSOA")
 public class Pessoa {
 
 	@Id
