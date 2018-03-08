58effdd572c6a0a239f0c8152917f1f775140c3c
victorminerva <victorminerva.m@gmail.com>
2018-03-07 21:07:42 -0300

    Put the serializable in pessoa model class

--- src/main/java/br/com/minervait/spring/model/Pessoa.java
+++ src/main/java/br/com/minervait/spring/model/Pessoa.java
@@ -1,12 +1,19 @@
 package br.com.minervait.spring.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "PESSOA")
-public class Pessoa {
+public class Pessoa implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
