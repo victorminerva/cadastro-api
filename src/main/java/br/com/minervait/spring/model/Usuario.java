34dc9fc51b0cffafc502dce99881a88fb2b8b538
victorminerva <victorminerva.m@gmail.com>
2018-03-15 21:20:19 -0300

    Include id field in Usuario object

--- src/main/java/br/com/minervait/spring/model/Usuario.java
+++ src/main/java/br/com/minervait/spring/model/Usuario.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
@@ -17,19 +19,31 @@ import javax.persistence.Id;
 public class Usuario implements Serializable {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+
 	@Column(name = "EMAIL")
 	private String email;
-	
+
 	@Column(name = "USUARIO_NOME", nullable = false)
 	private String usuarioNome;
-	
+
 	@Column(name = "SENHA", nullable = false)
 	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getUsuarioNome() {
 		return usuarioNome;
