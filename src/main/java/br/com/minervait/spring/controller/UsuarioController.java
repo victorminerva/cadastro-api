a53742c8d2572b4cdbc5a19cd014e8100c2a2b90
victorminerva <victorminerva.m@gmail.com>
2018-03-15 21:22:40 -0300

    Creating the UsuarioController

---
+++ src/main/java/br/com/minervait/spring/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package br.com.minervait.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.minervait.spring.model.Usuario;
+import br.com.minervait.spring.service.usuarioservice.UsuarioService;
+
+@RestController
+public class UsuarioController {
+
+	@Autowired
+	UsuarioService usuarioService;
+
+	@PostMapping("/usuario")
+	public ResponseEntity<?> registerNewUser(@RequestBody Usuario usuario) {
+		final Long id = usuarioService.registerNewUser(usuario);
+		return ResponseEntity.ok("User has been registered with Success! " + id);
+	}
+
+}
